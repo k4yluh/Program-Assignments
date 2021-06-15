cccb3d139cad85be3773e5d4681071c10215b120
Kayla Tran <kaylatran2021@gmail.com>
2021-06-15 09:22:42 -1000

    add python file practice vars

---
+++ practice_vars.py
@@ -0,0 +1,18 @@
+name = "LeBrown"
+# string
+age = 35
+# integer
+salary = 37.44
+# float
+plays_basketball = True
+# boolean
+jersey_number= "23"
+# string
+
+car_name = "Ford"
+x = 50
+my_first_name = "Kayla"
+
+print(type(car_name))
+print(type(x))
+print(type(my_first_name))
